f26072ba4f6263e89c17c16b077b07f440491959
Богдан <bohdansalo31@gmail.com>
2025-02-25 16:28:37 +0200

    completed

---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+// ============ 1
+// const friends = ["Mango", "Orange", "Kiwi"];
+// let elem = "";
+
+// for
+// for (let i = 0; i < friends.length; i++) {
+//   elem += `${friends[i]},
+//    `;
+// }
+
+// join
+// const elem = friends.join(",");
+
+// console.log(elem);
+
+// ============ 2
+
+const cards = [
+  "Карточка-1",
+  "Карточка-2",
+  "Карточка-3",
+  "Карточка-4",
+  "Карточка-5",
+];
+
+// 2.1
+
+// const deletedCards = "Карточка-3";
+// const cardToRemove = cards.indexOf(deletedCards);
+// cards.splice(cardToRemove, 1);
+
+// console.log(cards);
+
+// 2.2
+
+// const cardToInsert = "Карточка-6";
+// cards.push(cardToInsert);
+
+// console.log(cards);
+
+// 2.3
+
+const cardToUpdate = "Карточка-4";
+const cardUpdate = cards.indexOf(cardToUpdate);
+cards.splice(cardUpdate, 1, "Нова карточка-4");
+
+console.log(cards);
